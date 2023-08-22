24550fcc8183ad51e25b931c2a7e674515531774
SofianeT <89205501+SofianeT@users.noreply.github.com>
2023-08-22 17:19:55 +0200

    First commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const colors = require("colors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema.js");
+const connectDB = require("./config/db.js");
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`Server is running on port ${port}`));
